c0b72b344ea0e97bcb19c79b9db99dc9da85adb7
Ji Park <61026408+jiparkdev@users.noreply.github.com>
2020-04-20 22:36:56 -0700

    Solve the Sorted Union problem.

--- sorted-union.js
+++ sorted-union.js
@@ -24,8 +24,30 @@
 // uniteUnique([1, 2, 3], [5, 2, 1]) should return [1, 2, 3, 5].
 // uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]) should return [1, 2, 3, 5, 4, 6, 7, 8].
 
-function uniteUnique(arr) {
-  return arr;
+console.clear();
+
+function uniteUnique(...arr) {
+  let u = [];
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      if (u.indexOf(arr[i][j]) === -1) {
+        u.push(arr[i][j]);
+      }
+    }
+  }
+  return u;
 }
 
-uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+const res = [
+  uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]),
+  uniteUnique([1, 2, 3], [5, 2, 1]),
+  uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]),
+];
+
+console.log(res);
+
+/* Output
+
+  [ [ 1, 3, 2, 5, 4 ], [ 1, 2, 3, 5 ], [ 1, 2, 3, 5, 4, 6, 7, 8 ] ]
+
+*/
