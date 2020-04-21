e75f7effb1b26196f222c4b6fac46be5a98348fa
Ji Park <61026408+jiparkdev@users.noreply.github.com>
2020-04-20 23:11:27 -0700

    Solve and update Convert HTML Entities challenge.

--- convert-html-entities.js
+++ convert-html-entities.js
@@ -22,8 +22,69 @@
 // convertHTML("<>") should return &lt;&gt;.
 // convertHTML("abc") should return abc.
 
-function convertHTML(str) {
-  return str;
+console.clear();
+
+function numChars(char, str) {
+  const tempStr = str.slice();
+  // Get number of a char within a string.
+  let count = 0;
+  for (let i = 0; i < tempStr.length; i++) {
+    if (char === tempStr[i]) {
+      count++;
+    }
+  }
+  return count;
 }
 
-convertHTML("Dolce & Gabbana");
+function convertHTML(str) {
+  const entities = {
+    "&": "&amp;",
+    ">": "&gt;",
+    "<": "&lt;",
+    '"': "&quot;",
+    "'": "&apos;",
+  };
+  const chars = ["&", "<", ">", '"', "'"];
+  let tstr = str.slice();
+  for (let char of chars) {
+    let tracker = 0;
+    const count = numChars(char, tstr);
+    if (count === 0) {
+      continue;
+    }
+    do {
+      const idx = tstr.indexOf(char);
+      const str1 = tstr.slice(0, idx);
+      const str2 = tstr.slice(idx + 1);
+      tstr = str1 + entities[char] + str2;
+      tracker++;
+    } while (tracker < count);
+  }
+  return tstr;
+}
+
+const output = [
+  convertHTML("Dolce & Gabbana"),
+  convertHTML("Hamburgers < Pizza < Tacos"),
+  convertHTML("Sixty > twelve"),
+  convertHTML('Stuff in "quotation marks"'),
+  convertHTML("Schindler's List"),
+  convertHTML("<>"),
+  convertHTML("abc"),
+];
+
+console.log(output);
+
+/* Output
+
+[ 
+  'Dolce &amp; Gabbana',
+  'Hamburgers &lt; Pizza &lt; Tacos',
+  'Sixty &gt; twelve',
+  'Stuff in &quot;quotation marks&quot;',
+  'Schindler&apos;s List',
+  '&lt;&gt;',
+  'abc'
+]
+  
+*/
