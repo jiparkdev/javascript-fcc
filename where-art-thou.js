335b211549fe490db672f0e925e584f51dd18da8
Ji Park <61026408+jiparkdev@users.noreply.github.com>
2020-04-23 20:38:36 -0700

    Solve Wherefore Art Thou challenge.

--- where-art-thou.js
+++ where-art-thou.js
@@ -23,19 +23,29 @@
 // whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }, { "bat":2 }], { "apple": 1, "bat": 2 }) should return [{ "apple": 1, "bat": 2 }, { "apple": 1, "bat": 2, "cookie":2 }].
 // whatIsInAName([{"a": 1, "b": 2, "c": 3}], {"a": 1, "b": 9999, "c": 3}) should return []
 
+function srcInItem(item, source) {
+  // item = { first: "Tybalt", last: "Capulet" }
+  // source = { last: "Capulet" }
+  const sourceKeys = Object.keys(source);
+  for (let key of sourceKeys) {
+    if (!key in item) {
+      return false;
+    }
+  }
+  for (let key of sourceKeys) {
+    if (source[key] !== item[key]) {
+      return false;
+    }
+  }
+  return true;
+}
+
 function whatIsInAName(collection, source) {
   var arr = [];
   // Only change code below this line
-
+  arr = collection.filter((item) => {
+    return srcInItem(item, source);
+  });
   // Only change code above this line
   return arr;
 }
-
-whatIsInAName(
-  [
-    { first: "Romeo", last: "Montague" },
-    { first: "Mercutio", last: null },
-    { first: "Tybalt", last: "Capulet" },
-  ],
-  { last: "Capulet" }
-);
